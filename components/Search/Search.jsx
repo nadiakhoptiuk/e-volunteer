6c8709af3a342958f5e409bb868fea16a1172d6c
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-12-21 18:16:49 +0200

    upd

---
+++ components/Search/Search.jsx
@@ -0,0 +1,5 @@
+// import { useCallback, useRef, useState } from 'react';
+// import Link from 'next/link';
+// import { useRouter } from 'next/router';
+// import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+// import { XMarkIcon } from '@heroicons/react/24/solid';
