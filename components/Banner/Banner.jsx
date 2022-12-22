b492c639132ff111eb0c0c397bbc2fd87a3e6986
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-12-22 17:01:18 +0200

    pull from main

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,6 +1,8 @@
 const Banner = ({ banner }) => {
   return (
-    <h1 className="mb-10 text-center text-3xl font-bold underline" id="banner">{banner}</h1>
+    <h1 className="mb-10 text-center text-3xl font-bold underline" id="banner">
+      {banner}
+    </h1>
   );
 };
 
