f634ef3ab3c5c7c77bbf3b24aa23da4f84ad0746
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-12-23 16:39:23 +0200

    add content for page

--- pages/[category].js
+++ pages/[category].js
@@ -1,23 +1,39 @@
+import ReactMarkdown from 'react-markdown';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { categoryRequest, routeRequest } from '@/lib/datoCmsRequests';
-// import { useRouter } from 'next/router';
+import { Container } from '@/components';
+import { routes } from 'routes';
+import Link from 'next/link';
 
 const CategoryPage = props => {
-  // const router = useRouter();
-  // console.log(router);
-  // console.log(article);
   const { category } = props;
 
-  // const { category } = router.query;
+  console.log(category);
 
-  return <p>{category && category.route} </p>;
+  return (
+    <>
+      {category && (
+        <section>
+          <Container>
+            <div className="flex">
+              <Link href={routes.HOME} aria-label="button back home">
+                Back
+              </Link>
+
+              <h2 className="ml-20">{category.title}</h2>
+            </div>
+
+            <ReactMarkdown>{category.cardInfo[0].contentAtPage}</ReactMarkdown>
+          </Container>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default CategoryPage;
 
 export async function getStaticPaths() {
-  // const variables = { locale: locale };
-
   const data = await routeRequest();
 
   if (!data) {
@@ -38,7 +54,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ locale, params }) {
   const { category } = params;
 
-  const variables = { locale: locale };
+  const variables = { locale: locale, category: category };
 
   const data = await categoryRequest({ variables });
 
@@ -51,7 +67,7 @@ export async function getStaticProps({ locale, params }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
-      category: data.allCategories.find(el => el.route === category),
+      category: data.allCategories[0],
       banner: data.banner.content,
       help: data.help,
       footer: data.footer,
