537958f9929c3e75ea3c4d97b3172c68a686c9d2
Maryna Ostapliuk <marynaum01@gmail.com>
2022-12-25 22:20:19 +0200

    upd styles centres

--- components/index.js
+++ components/index.js
@@ -2,7 +2,6 @@ export * from './Container/Container';
 export * from './Logo/Logo';
 export * from './Navbar/Navbar';
 export * from './LanguageToggle/LanguageToggle';
-export * from '../views/Centers/Centers';
 export * from './Banner/Banner';
 export * from './ButtonLink/ButtonLink';
 export * from './Search/Search';
