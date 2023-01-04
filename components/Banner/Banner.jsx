b714a23d9b749e1a663d95770ad96b0d332e5175
Vasyl Yakymets <Vasia911119@gmail.com>
2023-01-04 15:15:12 +0200

    fix/banner

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from '..';
 import Close from '../../public/image/form/close.svg';
@@ -6,23 +6,40 @@ import * as s from './Banner.module.css';
 
 export const Banner = ({ data }) => {
   const [isOpen, setIsOpen] = useState(true);
+
+  useLayoutEffect(() => {
+    setIsOpen(
+      JSON.parse(
+        typeof window !== 'undefined' &&
+          (window.sessionStorage.getItem('open') ?? true),
+      ),
+    );
+  }, []);
+
   function closeBanner() {
+    typeof window !== 'undefined' &&
+      window.sessionStorage.setItem('open', JSON.stringify(false));
     setIsOpen(false);
   }
+
   return (
-    <div className={isOpen ? 'bg-blueLight' : 'visually-hidden'}>
-      <Container className={s.container}>
-        <h2 className={s.title}>{data}</h2>
-        <button
-          className={s.button}
-          onClick={closeBanner}
-          aria-label="banner close button"
-          type="button"
-        >
-          <Close className={s.close} />
-        </button>
-      </Container>
-    </div>
+    <>
+      {isOpen && (
+        <div className="bg-blueLight">
+          <Container className={s.container}>
+            <h2 className={s.title}>{data}</h2>
+            <button
+              className={s.button}
+              onClick={closeBanner}
+              aria-label="banner close button"
+              type="button"
+            >
+              <Close className={s.close} />
+            </button>
+          </Container>
+        </div>
+      )}
+    </>
   );
 };
 
