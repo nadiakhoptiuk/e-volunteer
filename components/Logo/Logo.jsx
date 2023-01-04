04c8d9c25d8dbea2a7df379ac00fb0237ce15d93
Maryna Ostapliuk <marynaum01@gmail.com>
2023-01-04 14:12:20 +0200

    fised focus i logo

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -1,16 +1,11 @@
 import Link from 'next/link';
 import { routes } from 'routes';
-import LogoSite from '../../public/image/logo.png';
-import Image from 'next/image';
+import LogoSite from '../../public/image/Logo.svg';
 
 export const Logo = () => {
   return (
     <Link href={routes.HOME} aria-label="site logo">
-      <Image
-        src={LogoSite}
-        alt="Логотип"
-        className="h-11 w-16 xl:h-[58px] xl:w-[86px]"
-      />
+      <LogoSite className="h-11 w-16 xl:h-[58px] xl:w-[86px]" />
     </Link>
   );
 };
